efcd097147114562b7fed61dc7a4874d74de21f1
jed lahrim <jolix1235@gmail.com>
2025-04-29 16:29:54 +0100

    fix s3 & reformat the code;

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,32 +1,32 @@
-import { Configuration, OpenAIApi } from "openai-edge";
-import { Message, OpenAIStream, StreamingTextResponse } from "ai";
-import { getContext } from "@/lib/context";
-import { db } from "@/lib/db";
-import { chats, messages as _messages } from "@/lib/db/schema";
-import { eq } from "drizzle-orm";
-import { NextResponse } from "next/server";
+import {Configuration, OpenAIApi} from "openai-edge";
+import {Message, OpenAIStream, StreamingTextResponse} from "ai";
+import {getContext} from "@/lib/context";
+import {db} from "@/lib/db";
+import {chats, messages as _messages} from "@/lib/db/schema";
+import {eq} from "drizzle-orm";
+import {NextResponse} from "next/server";
 
 export const runtime = "edge";
 
 const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+    apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(config);
 
 export async function POST(req: Request) {
-  try {
-    const { messages, chatId } = await req.json();
-    const _chats = await db.select().from(chats).where(eq(chats.id, chatId));
-    if (_chats.length != 1) {
-      return NextResponse.json({ error: "chat not found" }, { status: 404 });
-    }
-    const fileKey = _chats[0].fileKey;
-    const lastMessage = messages[messages.length - 1];
-    const context = await getContext(lastMessage.content, fileKey);
+    try {
+        const {messages, chatId} = await req.json();
+        const _chats = await db.select().from(chats).where(eq(chats.id, chatId));
+        if (_chats.length != 1) {
+            return NextResponse.json({error: "chat not found"}, {status: 404});
+        }
+        const fileKey = _chats[0].fileKey;
+        const lastMessage = messages[messages.length - 1];
+        const context = await getContext(lastMessage.content, fileKey);
 
-    const prompt = {
-      role: "system",
-      content: `AI assistant is a brand new, powerful, human-like artificial intelligence.
+        const prompt = {
+            role: "system",
+            content: `AI assistant is a brand new, powerful, human-like artificial intelligence.
       The traits of AI include expert knowledge, helpfulness, cleverness, and articulateness.
       AI is a well-behaved and well-mannered individual.
       AI is always friendly, kind, and inspiring, and he is eager to provide vivid and thoughtful responses to the user.
@@ -40,34 +40,35 @@ export async function POST(req: Request) {
       AI assistant will not apologize for previous responses, but instead will indicated new information was gained.
       AI assistant will not invent anything that is not drawn directly from the context.
       `,
-    };
+        };
 
-    const response = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [
-        prompt,
-        ...messages.filter((message: Message) => message.role === "user"),
-      ],
-      stream: true,
-    });
-    const stream = OpenAIStream(response, {
-      onStart: async () => {
-        // save user message into db
-        await db.insert(_messages).values({
-          chatId,
-          content: lastMessage.content,
-          role: "user",
+        const response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [
+                prompt,
+                ...messages.filter((message: Message) => message.role === "user"),
+            ],
+            stream: true,
         });
-      },
-      onCompletion: async (completion) => {
-        // save ai message into db
-        await db.insert(_messages).values({
-          chatId,
-          content: completion,
-          role: "system",
+        const stream = OpenAIStream(response, {
+            onStart: async () => {
+                // save user message into db
+                await db.insert(_messages).values({
+                    chatId,
+                    content: lastMessage.content,
+                    role: "user",
+                });
+            },
+            onCompletion: async (completion) => {
+                // save ai message into db
+                await db.insert(_messages).values({
+                    chatId,
+                    content: completion,
+                    role: "system",
+                });
+            },
         });
-      },
-    });
-    return new StreamingTextResponse(stream);
-  } catch (error) {}
+        return new StreamingTextResponse(stream);
+    } catch (error) {
+    }
 }
--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,10 +1,10 @@
-import { neon, neonConfig } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
+import {neon, neonConfig} from "@neondatabase/serverless";
+import {drizzle} from "drizzle-orm/neon-http";
 
 neonConfig.fetchConnectionCache = true;
 
 if (!process.env.DATABASE_URL) {
-  throw new Error("database url not found");
+    throw new Error("database url not found");
 }
 
 const sql = neon(process.env.DATABASE_URL);
